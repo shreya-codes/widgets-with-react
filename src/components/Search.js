0523f815eeb45c1148d67412a30f1c1add11ca89
Shreya Shrestha <shreya.shrestha1999@gmail.com>
2021-06-22 21:26:24 +0545

    dropdown

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,8 +3,10 @@ import axios from 'axios';
 
 const Search=()=>{ 
 
-    const[term , setTerm]=useState('');
+    const [term , setTerm]=useState('');
+    const [results,setResults] =useState([]);
 
+    console.log(results);
     // console.log("I only run with every render")
 
     // useEffect(()=>{
@@ -19,20 +21,26 @@ const Search=()=>{
 
     useEffect(()=>{
        const search=async()=>{
-            await axios.get('https://en.wikipedia.org/w/api.php',{
+           const {data}= await axios.get('https://en.wikipedia.org/w/api.php',{
             params: {
                 action:'query',
                 list:'search',
                 origin:'*',
                 format:'json',
-                srsearchh:term,
+                srsearch:term, 
             },
 
         });
+        setResults(data.query.search);
         };
+        if (term){
         search();
-        
-    },[term])    
+        }
+    },[term]);
+    
+    const renderedResults = results.map(()=>{
+
+    });
 
     
     return(
@@ -45,6 +53,7 @@ const Search=()=>{
                     onChange={(e)=>setTerm(e.target.value)}
                     className="input"
                     />
+                    
 
                 </div>
             </div>
